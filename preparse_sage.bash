4868b6db3d3b535da92e44149f17c419bb9c8e1f
Paweł Płatek <e2.8a.95@gmail.com>
2017-08-29 19:48:01 +0200

    update setup, add sage preparsing

---
+++ preparse_sage.bash
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+find . -name '*.sage' -exec sage --preparse {} \; -exec sh -c 'mv "$1.py" "${1%.sage}_sage.py"' _ {}  \;
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
+from distutils.command.build_py import build_py as _build_py
+from distutils.core import setup
+from subprocess import call
+
+class build_py(_build_py):
+    """Preparse and rename to .py all sage files to make them importable"""
+    print("Preparsing sage files")
+    call(['bash', './preparse_sage.bash'])
 
 setup(name='CryptoAttacks',
-      version='0.1',
-      description='Implementation of some cryptography attacks',
-      url='https://github.com/GrosQuildu/CryptoAttacks',
-      author='Gros Quildu',
-      author_email='e2.8a.95@gmail.com',
-      license='MIT',
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=['pycrypto', 'gmpy2', 'BeautifulSoup'])
+        version='0.1',
+        description='Implementation of some cryptography attacks',
+        url='https://github.com/GrosQuildu/CryptoAttacks',
+        author='Gros Quildu',
+        author_email='e2.8a.95@gmail.com',
+        license='MIT',
+        packages=find_packages(),
+        zip_safe=False,
+        cmdclass={'build_py': build_py},
+        install_requires=['future', 'pycrypto', 'gmpy2', 'BeautifulSoup', 'requests'])
